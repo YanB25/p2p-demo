f111711b3493f83eb9da3cea19487062cc2cad00
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-03-29 17:17:28 +0800

    init

--- src/helloQT.py
+++ src/helloQT.py
@@ -1,16 +1,32 @@
 import sys
 from PyQt4 import QtGui
+class Window(object):
+    def __init__(self):
+        self.app = QtGui.QApplication(sys.argv)
+        self.w = QtGui.QWidget()
+        self.b = QtGui.QLabel(self.w)
+        LABEL = "Internet Log"
+        self.b.setText(LABEL)
+        self.b.move(400, 20)
+        self.w.setGeometry(100,100,200,50)
+        self.w.setWindowTitle("P2P Demo")
 
-def window():
-   app = QtGui.QApplication(sys.argv)
-   w = QtGui.QWidget()
-   b = QtGui.QLabel(w)
-   b.setText("Hello World!")
-   w.setGeometry(100,100,200,50)
-   b.move(50,20)
-   w.setWindowTitle("PyQt")
-   w.show()
-   sys.exit(app.exec_())
-	
+        self.textbox = QtGui.QPlainTextEdit(self.w)
+        self.textbox.move(100, 50)
+        self.textbox.resize(600, 400)
+        self.textbox.setReadOnly(True)
+        self.w.resize(800, 500)
+        self.w.show()
+    def run(self):
+        self.w.show()
+        self.app.exec_()
+    def appendText(self, text):
+        print(text)
+        self.textbox.setPlainText(self.textbox.toPlainText() +text+ "\n")
+
 if __name__ == '__main__':
-   window()
+    window = Window()
+    window.appendText('hello')
+    window.appendText('world')
+    window.run()
+    
