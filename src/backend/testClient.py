f05e66b9dbe5bdfbefd7fafe7f9d4ae759c3dc49
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-04-06 16:58:00 +0800

    [milestone] finish framework in server side - server.py: finish server logic - testClient.py: test connect

--- src/backend/testClient.py
+++ src/backend/testClient.py
@@ -1,15 +1,24 @@
 import socket 
 import json
 import utilities
-data = {
-    'name': 'benson',
-    'age': 18
-}
-json_data = json.dumps(data)
+from server_config import *
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('127.0.0.1', 5005))
+s.connect(('127.0.0.1', SERVER_PORT))
 s.send(utilities.objEncode({
-    'name': 'benson',
-    'age' : 18
+    'ip': SERVER_IP,
+    'port' : 1000,
+    'id': 'asddsf',
+    'event': 'started'
+}))
+data = s.recv(1000)
+print(utilities.objDecode(data))
+for i in range(1000):
+    continue
+print('finish')
+s.send(utilities.objEncode({
+    'event': 'completed',
+    'ip': SERVER_IP,
+    'port': 1000,
+    'id': 'asddsf'
 }))
 s.close()
