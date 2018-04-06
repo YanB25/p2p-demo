d5e8b7988832494a8097ac328d2065a4a7c45846
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-04-06 17:07:25 +0800

    [fix] add static method directive - message.py: add @staticmethod in member function

--- src/backend/message.py
+++ src/backend/message.py
@@ -1,40 +1,49 @@
 """ create message  object """
 
 class Message():
+    @staticmethod
     def keep_alive():
         return ''
 
+    @staticmethod
     def choke():
         msg = create_message('choke')
         return msg
     
+    @staticmethod
     def no_choke():
         msg = create_message('no_choke')
         return msg
 
+    @staticmethod
     def interested():
         msg = create_message('interested')
         return msg
     
+    @staticmethod
     def no_interested():
         msg = create_message('no_interested')
         return msg
     
+    @staticmethod
     def have(piece_index):
         msg = create_message('have')
         msg['piece_index'] = piece_index
         return msg
     
+    @staticmethod
     def bitfield(_bitfield):
         msg = create_message('bitfield')
         msg['bitfield'] = _bitfield
         return msg
     
+    @staticmethod
     def resquest(piece_index):
         msg = create_message('resquest')
         msg['piece_index'] = piece_index
         return msg
     
+    @staticmethod
     def piece(piece_index, raw_data):
         msg = create_message('piece')
         msg['piece_index'] = piece_index
