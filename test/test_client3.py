42f8b8b3bfd10a2784d7dd140b241d98109ba03e
walker <WalkerYF@yeah.net>
2018-04-11 12:05:43 +0800

    edit test py file 这几个测试文件都会读取client1里面的torrent文件（因为那一个一定是最新的，每次运行都会更新）

--- test/test_client3.py
+++ test/test_client3.py
@@ -4,7 +4,7 @@ from client import *
 from torrent import *
 
 # make_torrent_file('vid.mp4')
-test_torrent_file = './vid.mp4.torrent'
+test_torrent_file = './client1/vid.mp4.torrent'
 test_config_file = './client3_config.json'
 client = Client(test_torrent_file,test_config_file)
 client.start()
