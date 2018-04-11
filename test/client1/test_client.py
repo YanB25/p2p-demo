ae586a8615157cce80a019f45f26257470614f87
walker <WalkerYF@yeah.net>
2018-04-11 22:12:59 +0800

    add a test example

--- test/client1/test_client.py
+++ test/client1/test_client.py
@@ -3,8 +3,9 @@ sys.path.insert(0, '../../src/backend/')
 from client import *
 from torrent import *
 
-make_torrent_file('vid.mp4')
-test_torrent_file = './vid.mp4.torrent'
+file_name = 'test.txt'
+make_torrent_file(file_name)
+test_torrent_file = file_name+'.torrent'
 test_config_file = './client_config.json'
 client = Client(test_torrent_file,test_config_file)
 client.start()
