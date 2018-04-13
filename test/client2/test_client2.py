2a9d906b7d5a9579f56454451971d51c134c9dc4
wyf <walkeryf@yeah.net>
2018-04-13 23:46:21 +0800

    移动了一些测试文件，以便做到不同客户端具有不同的数据块

---
+++ test/client2/test_client2.py
@@ -0,0 +1,11 @@
+import sys
+sys.path.insert(0, '../src/backend/')
+from client import *
+from torrent import *
+
+# make_torrent_file('vid.mp4')
+file_name = 'test.txt'
+test_torrent_file = './client1/{}.torrent'.format(file_name)
+test_config_file = './client2_config.json'
+client = Client(test_torrent_file,test_config_file)
+client.start()
