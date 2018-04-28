db66d186f031922e457a0b3e82191465d1799c3d
walker <WalkerYF@yeah.net>
2018-04-28 21:55:12 +0800

    add c5 c6

--- demo/seed/seed_client.py
+++ demo/seed/seed_client.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '../../src/backend/')
 from client import *
 from torrent import *
 
-file_name = '../vid.mp4'
+file_name = '../test.mp4'
 test_torrent_file = file_name+'.torrent'
 test_config_file = './client_config.json'
 client = Client(test_torrent_file,test_config_file)
