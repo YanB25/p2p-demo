e1d6f86400422fd749acd3936a00108aab77beb8
wangyf <walkeryf@yeah.net>
2018-04-29 22:38:55 +0800

    add test 8,9,10

--- demo/c8/test8_client.py
+++ demo/c8/test8_client.py
@@ -6,6 +6,6 @@ from torrent import *
 # make_torrent_file('vid.mp4')
 file_name = 'test.mp4'
 test_torrent_file = '../{}.torrent'.format(file_name)
-test_config_file = './client4_config.json'
+test_config_file = './client8_config.json'
 client = Client(test_torrent_file,test_config_file)
 client.start()
